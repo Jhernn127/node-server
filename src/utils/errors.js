92c0d3150f2eb661b74fce059eb87b11a77c654e
Jhernn127 <javi.hdz1426@gmail.com>
2023-11-27 09:06:54 -0600

    troubleshoot 2

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,6 +1,6 @@
 import logger from './logger'
 
-const notFound = (req, res, next) => {
+export const notFound = (req, res, next) => {
     const error = new Error (`Not Found - ${req.orinalURL}`)
     res.status(404)
     next(error)
